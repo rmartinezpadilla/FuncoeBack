f7ba7a92a34be6d2779d17cce89a34be96489c90
Ruben Martinez <rmartinezpadilla@hotmail.com>
2024-01-30 14:07:29 -0500

    students

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import advisor, document_type, concept, program
+from routes import advisor, document_type, concept, program, student
 
 app = FastAPI()
 
@@ -7,6 +7,7 @@ app.include_router(advisor.router)
 app.include_router(document_type.router)
 app.include_router(concept.router)
 app.include_router(program.router)
+app.include_router(student.router)
 
 
 @app.get("/")
---
+++ schemas/student.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, date
+
+class Student(BaseModel):
+    #modelo de pydantic que sirve para recibir los datos de entrada de la api
+    #en este caso lo utilizaremos para crear un estudiante
+    uuid_student:Optional[str] = None
+    document_type_uuid:str
+    identification_card:str
+    birthdate:date
+    first_name:str
+    last_name:str
+    municipality:str
+    address:str
+    phone:str
+    gender:str
+    blood_type:str
+    recommendation:str
+    advertising_medium:str    
+    day_uuid:str
+    created_at:Optional[datetime] = None
+    working_day:str
+    registration_number:int
+    advisor_uuid:str
+    updated_at:Optional[datetime] = None
+    # created_at:datetime
+    # updated_at:datetime
+   
