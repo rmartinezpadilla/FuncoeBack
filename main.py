4220fc25147560480278e4c72ebe2f5bfa6a6455
Ruben Martinez <rmartinezpadilla@hotmail.com>
2024-02-16 20:56:33 -0500

    add router user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import advisor, blood_type, document_type, concept, program, student, teacher, module, semester, enroll, payment, pensum, shifts, role
+from routes import advisor, blood_type, document_type, concept, program, student, teacher, module, semester, enroll, payment, pensum, shifts, role, user
 
 
 app = FastAPI()
@@ -18,6 +18,7 @@ app.include_router(pensum.router)
 app.include_router(shifts.router)
 app.include_router(role.router)
 app.include_router(blood_type.router)
+app.include_router(user.router)
 
 @app.get("/")
 async def root():
