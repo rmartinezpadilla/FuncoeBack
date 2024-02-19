b0fe2ddc29aaa369a3f548b24f83e3a87652b8d3
rmartinezpadilla <85368960+rmartinezpadilla@users.noreply.github.com>
2024-02-18 22:27:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ app.include_router(day.router)
 #     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1",port=8000, reload= True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0",port=8000, reload= True, log_level="info")
