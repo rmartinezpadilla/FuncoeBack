a5a517a184455417bd712c1524ec98ad09abaed1
Ruben Martinez <rmartinezpadilla@hotmail.com>
2024-02-23 23:33:08 -0500

    refactor user

--- app/routes/user.py
+++ app/routes/user.py
@@ -5,17 +5,16 @@ from app.schemas.user import User_login as user_schema_login
 from app.auth.auth_bearer import JWTBearer
 from app.config.db import get_db,Session
 from app.models.user import User as user_model
+from app.utils.func import user as user_funcitons
 from app.auth.auth_handler import signJWT
 import uuid
 from datetime import datetime
-import bcrypt
-
 
 router =  APIRouter(prefix='/users', tags=['Users'], responses={404 : {'message' : 'Not found'}})
 
 @router.post("/token")
 async def get_token(user: user_schema_login):
-    if check_user(user):
+    if user_funcitons.validate_user(user):
         return signJWT(user.user)
     return {
         "error": "Wrong login details or user inactive!"
@@ -25,25 +24,22 @@ async def get_token(user: user_schema_login):
 async def create_user(user_obj:user_schema):
     try:#instrucción try, atrapa de inicio a fin las lineas que intentaremos ejecutar y que tiene posibilidad de fallar
     #¡inicio try!
-        session = get_db()
-        db:Session
-        for db in session:
-            #advisor_obj["id"] = uuid.uuid4() 
-            # print(type('esto es', advisor_obj))           
-            user_obj = user_model(**user_obj.model_dump())  
-            user_obj.uuid_user = uuid.uuid4()
-            salt = bcrypt.gensalt()
-            ashed = bcrypt.hashpw(user_obj.password.encode('utf-8'), salt)
-            user_obj.password=ashed
-            user_obj.created_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')          
-            #añade el recurso persona para subirse a la base de datos
-            db.add(user_obj)
-            #se sube a la base de datos
-            db.commit()
-            #se refresca la información en la variable persona para poderla devolver
-            #en el servicio
-            db.refresh(user_obj)
-            return user_obj
+            session = get_db()
+            db:Session
+            for db in session:          
+                user_obj = user_model(**user_obj.model_dump()) 
+                user_obj.uuid_user = uuid.uuid4()                 
+                user_obj.password = user_funcitons.encrypt_password(user_obj.password)
+                user_obj.created_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')                                        
+                user_obj.created_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')          
+                #añade el recurso persona para subirse a la base de datos
+                db.add(user_obj)
+                #se sube a la base de datos
+                db.commit()
+                #se refresca la información en la variable persona para poderla devolver
+                #en el servicio
+                db.refresh(user_obj)
+                return user_obj
     #¡fin try!
     except Exception as e: #instrucción que nos ayuda a atrapar la excepción que ocurre cuando alguna instrucción dentro de try falla
         #se debe controlar siempre que nos conectamos a una base de datos con un try - except
@@ -137,38 +133,51 @@ async def read_user(uuid_user: str):
         #la instrucción raise es similar a la instrucción return, pero en vez de retornar cualquier elemento, retornamos especificamente
         #un error, en este caso el error esta contenido en HTTPException
 
-@router.patch("/state_change/{uuid_user}", dependencies=[Depends(JWTBearer())], response_model = user_schema_response)
-async def change_status_user(user_uuid: str):
+@router.patch("/activate_user", dependencies=[Depends(JWTBearer())], response_model = user_schema_response)
+async def activate_status_user(user_uuid: str):
     try:#instrucción try, atrapa de inicio a fin las lineas que intentaremos ejecutar y que tiene posibilidad de fallar
     #¡inicio try!
         session = get_db()
         db:Session
-        for db in session:
-            #session.query(advisor_model).filter(adv_models.uuid_advisor == advisor_uuid).update(advisor_model)
-            #db.execute(adv_models).update().values(advisor_model_2)).where(adv_models.uuid_advisor == adv_uuid)
-            #advisor_model_2 = adv_models(**advisor_model_2.model_dump())
-            r = db.query(user_model).where(user_model.uuid_user == user_uuid).first()        
-            if r is not None:
-                if r.is_active == 1:
-                    r.is_active = 0
-                else:
-                    r.is_active = 1
+        for db in session:            
+            r = db.query(user_model).where(user_model.uuid_user == user_uuid).first()                        
+            if r is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='el id del usuario no existe')
+            elif r.is_active == 1:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= f'User {user_uuid} is active')
+            else:
+                r.is_active = 1
                 r.updated_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
                 db.commit()
                 db.refresh(r)
                 return r
-            else:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='el id del usuario no existe')
     
-    except Exception as e:#instrucción que nos ayuda a atrapar la excepción que ocurre cuando alguna instrucción dentro de try falla
-        #se debe controlar siempre que nos conectamos a una base de datos con un try - except
-        #debido a que no podemos controlar la respuesta del servicio externo (en este caso la base de datos)
-        #y es muy posible que la conexión falle por lo cual debemos responder que paso
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))
-        #la instrucción raise es similar a la instrucción return, pero en vez de retornar cualquier elemento, retornamos especificamente
-        #un error, en este caso el error esta contenido en HTTPException
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))        
 
-@router.delete("/{uuid_user}", dependencies=[Depends(JWTBearer())])
+@router.patch("/desactivate_user", dependencies=[Depends(JWTBearer())], response_model = user_schema_response)
+async def desactivate_status_user(user_uuid: str):
+    try:#instrucción try, atrapa de inicio a fin las lineas que intentaremos ejecutar y que tiene posibilidad de fallar
+    #¡inicio try!
+        session = get_db()
+        db:Session
+        for db in session:            
+            r = db.query(user_model).where(user_model.uuid_user == user_uuid).first()                        
+            if r is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='el id del usuario no existe')
+            elif r.is_active == 0:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= f'User {user_uuid} is desactive')
+            else:
+                r.is_active = 0
+                r.updated_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                db.commit()
+                db.refresh(r)
+                return r
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))
+
+@router.delete("/delete", dependencies=[Depends(JWTBearer())])
 async def delete_user(uuid_user: str):
     try:#instrucción try, atrapa de inicio a fin las lineas que intentaremos ejecutar y que tiene posibilidad de fallar
     #¡inicio try!
@@ -203,13 +212,4 @@ async def delete_user(uuid_user: str):
     #         return True
     # return False
 
-def check_user(data: user_schema_login):
-    session = get_db()
-    db:Session
-    for db in session:
-        r1=db.query(user_model).where(user_model.user==data.user).first()
-        r2=db.query(user_model).where(user_model.password==data.password).first()        
-        if r1 is not None and r2 is not None and r1.is_active == 1:
-            return True
-        else:
-            return False
+
---
+++ app/utils/func/user.py
@@ -0,0 +1,40 @@
+from cryptography.fernet import Fernet
+from app.schemas.user import User_login as user_schema_login
+from app.models.user import User as user_model
+from app.config.db import get_db,Session
+import base64
+
+# key = Fernet.generate_key()
+# fernet = Fernet(key)
+
+def encrypt_password(password : str):
+    #enpassword = fernet.encrypt(password.encode('utf-8'))        
+    enpassword = base64.b64encode(password.encode('utf-8'))
+    return enpassword
+
+def decrypt_password(password :  str):    
+    #decryp_pass = fernet.decrypt(password).decode('utf-8')
+    decryp_pass = base64.b64decode(password).decode('utf-8')
+    return decryp_pass    
+
+def validate_user(data: user_schema_login):
+    id_user = get_uuid_user(data.user)    
+    session = get_db()    
+    db:Session
+    for db in session:        
+        r1=db.query(user_model).where(user_model.uuid_user==id_user).one()
+        pass_decrypt = decrypt_password(r1.password)
+        if r1 is not None and data.password == pass_decrypt and r1.is_active == 1:
+            return True
+        else:
+            return False
+        
+def get_uuid_user(user:str):
+    session = get_db()
+    db:Session
+    for db in session:        
+        r1=db.query(user_model).where(user_model.user==user).one()
+        if r1 is not None:
+            return r1.uuid_user
+        else:
+            return None
